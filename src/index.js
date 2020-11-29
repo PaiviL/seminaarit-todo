6a4a957feb7dac6066b85aacbeebd44db470a190
Paivi Lampinen <bet983@myy.haaga-helia.fi>
2020-11-29 22:13:04 +0200

    fix commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const url = 'mongodb://127.0.0.1:27017/tek-seminaarit';
 const Todo = require('./models/Todo');
 
+// Creating connection to db and testing
 mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true })
 
 const db = mongoose.connection;
@@ -16,18 +17,18 @@ db.once('open', _ => {
     console.log('Database connected:', url)
 })
 
+// body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// Express functions and database calls
+// Front page
 app.get('/', function (req, res) {
-
     res.sendFile(__dirname + '/template/welcomeForm.html');
 })
 
+// JSON-listing
 app.get('/todolist', function (req, res) {
-
     let { priority } = req.query;
-
     if (priority === undefined) {   // no parameter given
         Todo.find( function(error, todos) { 
             if (error) console.error(error)
@@ -43,6 +44,7 @@ app.get('/todolist', function (req, res) {
     }
 })
 
+// Add to-do
 app.post('/todos', function (req, res) {
     console.log(req.body);
 
@@ -50,7 +52,6 @@ app.post('/todos', function (req, res) {
         priority: req.body.priority, 
         todo: req.body.todo,
     });
-
     todo.save(function (error, savedTodo) {
         if (error) console.error(error)
         console.log(savedTodo)
@@ -58,11 +59,12 @@ app.post('/todos', function (req, res) {
     })
 })
 
+// Delete-form
 app.get('/deletetodo', function (req, res) {
-
     res.sendFile(__dirname + '/template/deleteForm.html');
 })
 
+// Delete to-do
 app.post('/deletetodos', function (req, res) {
     console.log(req.body);
 
--- src/models/Todo.js
+++ src/models/Todo.js
@@ -7,9 +7,4 @@ const todoSchema = new mongoose.Schema({
 
 const Todo = mongoose.model('Todo', todoSchema);
 
-// todoSchema.statics.findAll = async function () {
-//     const todos = await find()
-//     console.log(todos);
-// }
-
 module.exports = mongoose.model('Todo', todoSchema);
